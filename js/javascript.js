6293bf17c15f6f299e9fb2c5e4930ea27d69c0ac
Kei <barcelogreison@gmail.com>
2025-05-06 22:58:35 +0800

    Connect four js study for my tictactoe

--- js/javascript.js
+++ js/javascript.js
@@ -2,4 +2,14 @@ function GameBoard() {
     const rows = 3;
     const columns = 3;
     const board = [];
+
+    for(let i = 1; i <= rows; i++) {
+        board[i] = [];
+        for(let j = 1; j <= columns; j++) {
+            board[i][j] = [];
+        }
+    }
+
+    return board
 }
+console.table(GameBoard());
